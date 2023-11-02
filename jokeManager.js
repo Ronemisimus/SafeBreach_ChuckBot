8d0a18c1accdf911fbdd43f50645a8cffcf6e5cf
Ronen Margolin <roni948576@gmail.com>
2023-11-02 22:12:49 +0200

    html is getting correctly from the site

--- jokeManager.js
+++ jokeManager.js
@@ -1,6 +1,9 @@
 const source = "https://parade.com/968666/parade/chuck-norris-jokes"
 
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-extra') 
+// Add stealth plugin and use defaults 
+const pluginStealth = require('puppeteer-extra-plugin-stealth') 
+const {executablePath} = require('puppeteer');
 const cheerio = require('cheerio')
 
 class JokeManager {
@@ -10,13 +13,37 @@ class JokeManager {
 
     async init() {
         try{
-            const browser = await puppeteer.launch();
+            puppeteer.use(pluginStealth())
+            const browser = await puppeteer.launch({ headless:false, executablePath: executablePath() });
             const page = await browser.newPage();
+            
+            await page.setRequestInterception(true); 
+            page.on('request', async (request) => { 
+                if (request.resourceType() == 'image') { 
+                    await request.abort(); 
+                } else { 
+                    await request.continue(); 
+                } 
+            });
+
+            await page.setExtraHTTPHeaders({ 
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
+                'upgrade-insecure-requests': '1', 
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8', 
+                'accept-encoding': 'gzip, deflate, br', 
+                'accept-language': 'en-US,en;q=0.9,en;q=0.8' 
+            });
+
+            await page.waitForTimeout(1000) 
+
             await page.goto(source);
 
-            await page.waitForSelector('div');
+            await page.screenshot({ path: 'image.png', fullPage: true });
 
             const html = await page.content();
+
+            await browser.close();
+
             this.avaliable_jokes = this.proccess(html)
         }
         catch (err){
