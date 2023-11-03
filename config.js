ed8b37399b6353fcf8d81e2f6eaa484030df1536
Ronen Margolin <roni948576@gmail.com>
2023-11-03 13:57:03 +0200

    added config with github secrets connection

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    translator_api_key: process.env.TRANSLATOR_API_KEY,
+    region: process.env.REGION,
+    entrypoint: process.env.ENTRYPOINT,
+    telegram_bot_token: process.env.TELEGRAM_BOT_TOKEN
+};
+
+export default config;
