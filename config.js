922dd7f8a197a5e6b9a29a3825b15b545c933582
Ronen Margolin <roni948576@gmail.com>
2023-11-03 15:19:59 +0200

    basic server, prevent azure shut down

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const config = {
     translator_api_key: process.env.TRANSLATOR_API_KEY,
     region: process.env.REGION,
     entrypoint: process.env.ENTRYPOINT,
-    telegram_bot_token: process.env.TELEGRAM_BOT_TOKEN
+    telegram_bot_token: process.env.TELEGRAM_BOT_TOKEN,
+    port: process.env.PORT || 3000
 };
 
 export default config;
--- index.js
+++ index.js
@@ -2,8 +2,31 @@ import  translate from './translate.js'
 import languageManager from './languageManager.js'
 import JokeManager from './jokeManager.js'
 import bot from './bot.js'
+import http from 'http'
+
+async function start_server_for_azure() {
+    
+
+    const port = process.env.PORT || 8080;
+
+    const server = http.createServer((req, res) => {
+        if (req.url=='/env'){
+            res.end(JSON.stringify(process.env))
+        }
+        else {
+            res.end('Hello World!')
+        }
+    });
+
+    server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+    });
+
+}
 
 async function main() {
+
+    start_server_for_azure()
 
     const manager = new languageManager.LanguageManager()
     const translator = new translate.Translator()
