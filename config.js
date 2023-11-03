d50d4e7873d935448ed4ab5ce8cba82cb5feb759
Ronen Margolin <roni948576@gmail.com>
2023-11-03 15:55:55 +0200

    removed webapp deployment - not fitting for bot

--- config.js
+++ config.js
@@ -2,8 +2,7 @@ const config = {
     translator_api_key: process.env.TRANSLATOR_API_KEY,
     region: process.env.REGION,
     entrypoint: process.env.ENTRYPOINT,
-    telegram_bot_token: process.env.TELEGRAM_BOT_TOKEN,
-    port: process.env.PORT || 3000
+    telegram_bot_token: process.env.TELEGRAM_BOT_TOKEN
 };
 
 export default config;
